bc8141042f07d0d214f97d059ce910b41e93e3ec
Nicholas Gavrilovic <168gdkop@students.codeinstitute.net>
2024-10-10 14:20:07 +0000

    Add division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,10 +39,14 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
+
 
     runGame(calculatedAnswer[1]);
 }
@@ -74,6 +78,7 @@ function calculateCorrectAnswer() {
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById("operator").innerText;
+    
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
@@ -81,6 +86,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-"){
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/"){
+        return [Math.floor(operand1 / operand2), "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -123,5 +130,11 @@ function displaySubtractQuestion(operand1, operand2) {
 function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
- 
+    document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
